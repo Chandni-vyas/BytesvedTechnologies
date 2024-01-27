1a42b911ddda55f4cf68f134a5a79f09ac6da565
Chandni-vyas <155969461+Chandni-vyas@users.noreply.github.com>
2024-01-27 07:54:10 +0530

    privacypolicy page

---
+++ privacypolicy.jsx
@@ -0,0 +1,259 @@
+import React from "react";
+import PageBanner from "@/src/components/PageBanner";
+import Layouts from "@/src/layouts/Layouts";
+
+const PrivacyPolicy = () => {
+  return (
+    <Layouts>
+      <PageBanner
+        pageTitle={"Privacy Policy"}
+        breadTitle={"Privacy Policy"}
+        // anchorLabel={"Send message"}
+        // anchorLink={"#contact"}
+        paddingBottom={1}
+        align={"center"}
+      />
+      {/* privacy-policy start*/}
+      <div className="privacy-policy">
+        <div className="main-head">
+          <div className="left-side">
+            <h4 className="pp-heading">
+              Collection of your Personal Information
+            </h4>
+            <p className="pp-paragraph">
+              bytesved.com strives to provide an interactive and personalised
+              experience to its visitors.
+              <br></br>
+              We may use personally identifiable information (such as your name
+              and email address) subject to the terms of this privacy policy. We
+              hate spam as much as you do and we will never sell, barter, or
+              rent your email address to any unauthorised third party. Like many
+              other Web sites, bytesved.com uses log files. The information
+              inside the log files includes internet protocol (IP) addresses,
+              type of browser, Internet Service Provider (ISP), date/time stamp,
+              referring/exit pages, and number of clicks to analyse trends,
+              administer the site, track user’s movement around the site, and
+              gather demographic information. IP addresses and other such
+              information are not linked to any information that is personally
+              identifiable.
+              <br></br>
+              As we adopt additional technology, we may also gather information
+              through other means. In certain cases, you can choose not to
+              provide us with information, for example by setting your browser
+              to refuse to accept cookies, but this may result in being unable
+              to access certain portions of the site or you may be asked to
+              re-enter your user name and password.
+            </p>
+          </div>
+          <div className="right-side">
+            <h4 className="pp-heading">Table of Contents</h4>
+            <div>
+              <a href="#personal_Info">Use of your Personal Information</a>
+            </div>
+            <div>
+              <a href="#use_of_Cookies">Use of Cookies</a>
+            </div>
+            <div>
+              <a href="#google_Analytics">Google Analytics</a>
+            </div>
+            <div>
+              <a href="#changes_Statement">Changes to this Statement</a>
+            </div>
+            <div>
+              <a href="#communication">Communication</a>
+            </div>
+            <div>
+              <a href="#links">Links To Other Sites</a>
+            </div>
+            <div>
+              <a href="#newsletter">Newsletter</a>
+            </div>
+            <div>
+              <a href="#blogs">Blogs</a>
+            </div>
+            <div>
+              <a href="#contact">Contact</a>
+            </div>
+            <div>
+              <a href="#consent">Consent</a>
+            </div>
+            <div>
+              <a href="#update">Update</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="personal_Info" id="personal_Info">
+          <h4 className="pp-heading">Use of your Personal Information</h4>
+          <p className="pp-paragraph">
+            bytesved.com collects and uses your personal information to operate
+            its website(s) and deliver the services you have requested.
+            <br></br>
+            bytesved.com may also use your personally identifiable information
+            to inform you of other products or services available from
+            bytesved.com and its affiliates. bytesved.com may also contact you
+            via surveys to conduct research about your opinion of current
+            services or of potential new services that may be offered.
+            bytesved.com does not sell, rent or lease its customer lists to
+            third parties.
+            <br></br>
+            bytesved.com may share data with trusted partners to help perform
+            statistical analysis, send you email or postal mail, provide
+            customer support, or arrange for deliveries. All such third parties
+            are prohibited from using your personal information except to
+            provide these services to bytesved.com, and they are required to
+            maintain the confidentiality of your information.
+            <br></br>
+            bytesved.com may keep track of the websites and pages our users
+            visit within bytesved.com, in order to determine what bytesved.com
+            services are the most popular.
+            <br></br>
+            This data is used to deliver customised content and advertising
+            within bytesved.com to customers whose behaviour indicates that they
+            are interested in a particular subject area.
+            <br></br>
+            bytesved.com will disclose your personal information, without
+            notice, only if required to do so by law or in the good faith belief
+            that such action is necessary to: (a) conform to the edicts of the
+            law or comply with legal process served on bytesved.com or the site;
+            (b) protect and defend the rights or property of bytesved.com; and,
+            (c) act under exigent circumstances to protect the personal safety
+            of users of bytesved.com, or the public.
+          </p>
+        </div>
+        <div className="use_of_Cookies" id="use_of_Cookies">
+          <h4 className="pp-heading">Use of Cookies</h4>
+          <p className="pp-paragraph">
+            Cookies are files with a small amount of data, which may include an
+            anonymous unique identifier. Cookies are sent to your browser from a
+            web site and transferred to your device. We use cookies to collect
+            information in order to improve our services for you.
+            <br></br>
+            You can instruct your browser to refuse all cookies or to indicate
+            when a cookie is being sent. The Help feature on most browsers
+            provides information on how to accept cookies, disable cookies or to
+            notify you when receiving a new cookie.
+            <br></br>
+            If you do not accept cookies, you may not be able to use some
+            features of our Service and we recommend that you leave them turned
+            on.
+          </p>
+        </div>
+        <div className="google_Analytics" id="google_Analytics">
+          <h4 className="pp-heading">Google Analytics</h4>
+          <p className="pp-paragraph">
+            bytesved.com uses Google Analytics to allow us to see how people are
+            interacting with our apps. For example, we can see that a certain
+            recipe is really popular, or that one is rarely viewed. This allows
+            us to improve the kind of recipes we provide. The important thing to
+            note here is that no personally identifiable information (including,
+            but not limited to: name, email address or unique device ID) is
+            transferred to Google. You can view Google’s privacy policy which
+            explains how they use your data. We also give people the option of
+            opting out of Google Analytics inside the about page of our apps.
+          </p>
+        </div>
+        <div className="changes_Statement" id="changes_Statement">
+          <h4 className="pp-heading">Changes to this Statement</h4>
+          <p className="pp-paragraph">
+            bytesved.com will occasionally update this Statement of Privacy to
+            reflect company and customer feedback. bytesved.com encourages you
+            to periodically review this Statement to be informed of how
+            bytesved.com is protecting your information.
+          </p>
+        </div>
+        <div className="communication" id="communication">
+          <h4 className="pp-heading">Communication</h4>
+          <p className="pp-paragraph">
+            bytesved.com will occasionally update this Statement of Privacy to
+            reflect company and customer feedback. bytesved.com encourages you
+            to periodically review this Statement to be informed of how
+            bytesved.com is protecting your information.
+          </p>
+        </div>
+        <div className="links" id="links">
+          <h4 className="pp-heading">Links To Other Sites</h4>
+          <p className="pp-paragraph">
+            Our Service may contain links to other sites that are not operated
+            by us. If you click on a third party link, you will be directed to
+            that third party’s site. We strongly advise you to review the
+            Privacy Policy of every site you visit. We have no control over, and
+            assume no responsibility for the content, privacy policies or
+            practices of any third party sites or services.
+          </p>
+        </div>
+        <div className="newsletter" id="newsletter">
+          <h4 className="pp-heading">Newsletter</h4>
+          <p className="pp-paragraph">
+            We may provide the opportunity to sign-up for a periodic email
+            newsletter by entering in your email address and confirming it by
+            clicking on a confirmation link sent to you by us by email (double
+            opt-in). The legal basis for this processing is Art. 6(1)(a) GDPR.
+            <br></br>
+            In some cases we may ask you for further information, such as name,
+            occupation, address or telephone number. Regarding the processing of
+            this personal data you will be asked to grant your explicit consent.
+            The legal basis for this processing is Art. 6(1)(a) GDPR.
+            <br></br>
+            If you subscribe for our periodic email newsletter your IP address
+            as well as data and time of subscription will be collected and
+            stored. This allows us to prevent misuse of our services or of the
+            e-mail address of the subscriber. The legal basis for this
+            processing is Art. 6(1)(f) GDPR.
+            <br></br>
+            This data is only used for the distribution of the newsletter. The
+            subscription may be terminated by you at any time by following the
+            unsubscribe instructions included in each newsletter. We will delete
+            the relevant personal data promptly upon your unsubscribe
+            instruction.
+          </p>
+        </div>
+        <div className="blogs" id="blogs">
+          <h4 className="pp-heading">Blogs</h4>
+          <p className="pp-paragraph">
+            Our website offers publicly accessible blogs. You should be aware
+            that any information you provide in these areas may be read,
+            collected and used by others who read them.
+          </p>
+        </div>
+        <div className="contact" id="contact">
+          <h4 className="pp-heading">Contact</h4>
+          <p className="pp-paragraph">
+            On our website we use several contact forms which can be used to get
+            in contact with us electronically. To handle your request it is
+            necessary to provide us with a valid email address, and, in
+            addition, we store your IP address and the time of your request to
+            prevent any misuse of our contact forms. Alternatively, you may
+            contact us via the email addresses cited. If you use one of these
+            channels, the personal data transmitted will be stored automatically
+            by bytesved.com as a data controller to handle our contact with you.
+            The legal basis for this processing is Art. 6(1)(f) GDPR.
+            <br></br>
+            In some cases we may ask you for further information, such as name,
+            occupation, address or telephone number. Regarding the processing of
+            this personal data you will be asked to grant your explicit consent.
+            The legal basis for this processing is Art. 6(1)(a) GDPR.
+          </p>
+        </div>
+        <div className="consent" id="consent">
+          <h4 className="pp-heading">Consent</h4>
+          <p className="pp-paragraph">
+            By using our website, you hereby consent to our privacy policy and
+            agree to its terms.
+          </p>
+        </div>
+        <div className="row12-side2" id="update">
+          <h4 className="pp-heading">Update</h4>
+          <p className="pp-paragraph">
+            This Privacy Policy was last updated on: 1 November, 2023.
+            <br></br>
+            Should we update, amend or make any changes to our privacy policy,
+            those changes will be posted here.
+          </p>
+        </div>
+      </div>
+      {/* privacy-policy end*/}
+    </Layouts>
+  );
+};
+export default PrivacyPolicy;
